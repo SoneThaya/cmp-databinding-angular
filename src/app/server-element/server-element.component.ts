664be4ebd120e5c5fa9221a705d7ca208cda2630
Sone <sthaya@gmail.com>
2021-03-01 20:52:26 -0800

    ng content

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
+import { Content } from "@angular/compiler/src/render3/r3_ast";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild,
+} from "@angular/core";
 
 @Component({
   selector: "app-server-element",
@@ -6,10 +23,81 @@ import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
   styleUrls: ["./server-element.component.css"],
   encapsulation: ViewEncapsulation.Emulated, // or ShadowDom
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy,
+    ViewChild,
+    ContentChild {
   @Input("srvElement") element: { type: string; name: string; content: string };
+  @Input() name: string;
+  @ViewChild("heading", { static: true }) header: ElementRef;
+  @ContentChild("contentParagraph") paragraph: ElementRef;
 
-  constructor() {}
+  constructor() {
+    console.log("constructor called!");
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges");
+    console.log(changes);
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log("ngOnInit called!");
+    console.log("Text Content: " + this.header.nativeElement.textContent);
+    console.log(
+      "Text Content of Paragraph: " + this.paragraph.nativeElement.textContent
+    );
+  }
+
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log("ngDoCheck called!");
+  }
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log("ngAfterContentInit called!");
+    console.log(
+      "Text Content of Paragraph: " + this.paragraph.nativeElement.textContent
+    );
+  }
+
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    console.log("ngAfterContentChecked called!");
+  }
+
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    console.log("ngAfterViewChecked called!");
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log("ngAfterViewInit called!");
+    console.log("Text Content: " + this.header.nativeElement.textContent);
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    console.log("ngOnDestroy called!");
+  }
 }
